07b99d17bb255603c512cb55d6bb46284ff08afc
prvnlhr <praveenlohar04@gmail.com>
2021-11-24 22:55:22 +0530

    first commit of java dsa practice

---
+++ Practice/graphs/Prindddt_Graph.java
@@ -0,0 +1,30 @@
+package graphs;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Prindddt_Graph {
+	
+	public static List<List<String>> groupAnagrams(String[] strings){
+		return null;
+		
+	}
+	
+	public static int helloWorld() {
+		int a = 5;
+		int b = 10;
+		int c  = a + b;
+		
+		Map<String,List<String>> map = new HashMap<>();
+			
+		
+		return 5;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hey there");
+	}
+
+}
